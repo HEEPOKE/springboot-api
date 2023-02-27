38279069c49e1643db3cc176a6e24a781b6d6ce6
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-02-27 13:53:50 +0700

    :money_with_wings: config connect db

--- demo/src/main/java/app/demo/config/DatabaseConnection.java
+++ demo/src/main/java/app/demo/config/DatabaseConnection.java
@@ -1,25 +1,25 @@
 import java.sql.*;
+import java.util.*;
 
 public class DatabaseConnection {
     
-    public static void main(String[] args) {
-        
-        Connection connection = null;
-        
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+    private static final String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+    private static final String DB_URL = System.getenv("DB_URL");
+    private static final String DB_USERNAME = System.getenv("DB_USERNAME");
+    private static final String DB_PASSWORD = System.getenv("DB_PASSWORD");
 
-            String url = "jdbc:mysql://localhost:3306/spring";
-            String username = "root";
-            String password = "";
-            
-            connection = DriverManager.getConnection(url, username, password);
-            
+    public static void main(String[] args) {
+
+        try {
+            Class.forName(DRIVER_NAME);
+
+            Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+
             connection.close();
         } catch (ClassNotFoundException e) {
             System.err.println("Could not load database driver: " + e.getMessage());
         } catch (SQLException e) {
             System.err.println("Could not connect to the database: " + e.getMessage());
-        }
-    }
-}
+        } 											
+    }                                               
+}             
