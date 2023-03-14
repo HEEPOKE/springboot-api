cd7b3b3666571dfc64848595546c90264e9b6593
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-03-14 17:25:05 +0700

    :money_with_wings: clear

--- app/src/main/java/com/example/app/DemoApplication.java
+++ app/src/main/java/com/example/app/DemoApplication.java
@@ -1,15 +1,15 @@
-package com.example.app;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import(Database.class)
+@ComponentScan("com.example.app")
 public class DemoApplication {
-
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
+    
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+    
 }
--- app/src/main/java/com/example/app/services/MainService.java
+++ app/src/main/java/com/example/app/services/MainService.java
@@ -2,11 +2,13 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Service
 public class MainService {
+    
     private final DataSource dataSource;
-
+    
     @Autowired
-    public MyService(DataSource dataSource) {
+    public MainService(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 }
