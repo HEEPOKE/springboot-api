be18a4ce7960c2f26e7d45e2504535f5e13d8219
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-03-15 14:13:07 +0700

    :money_with_wings: add v.

--- app/src/main/java/com/example/app/config/Database.java
+++ app/src/main/java/com/example/app/config/Database.java
@@ -1,14 +1,33 @@
 package com.example.app.config;
 
 import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Database {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
     @Bean
     public DataSource dataSource() {
-        return DataSourceBuilder.create().build();
+        return DataSourceBuilder.create()
+                .url(url)
+                .username(username)
+                .password(password)
+                .driverClassName(driverClassName)
+                .build();
     }
 }
