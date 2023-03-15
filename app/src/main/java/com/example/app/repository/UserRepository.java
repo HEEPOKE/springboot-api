1999426160afa78420c4e78936e4ff3de74dbd7c
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-03-15 12:47:15 +0700

    :money_with_wings: import entiites

--- app/src/main/java/com/example/app/repository/UserRepository.java
+++ app/src/main/java/com/example/app/repository/UserRepository.java
@@ -1,18 +1,7 @@
-package com.example.app;
+package com.example.app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import com.example.app.entities.User;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
-
-
-
-
-
-
-
-
-
-
--- app/src/main/java/com/example/app/services/MainService.java
+++ app/src/main/java/com/example/app/services/MainService.java
@@ -1,13 +1,11 @@
 package com.example.app.services;
 
 import javax.sql.DataSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MainService {
     
-    @Autowired
     public MainService(DataSource dataSource) {
     }
 }
