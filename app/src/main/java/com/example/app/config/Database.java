e30afc5acd2efded3b3caac278595278041d0834
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-03-15 09:13:59 +0700

    :money_with_wings: edit gradle

--- app/src/main/java/com/example/app/config/Database.java
+++ app/src/main/java/com/example/app/config/Database.java
@@ -3,15 +3,12 @@ package com.example.app.config;
 import javax.sql.DataSource;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class Database {
     @Bean
     public DataSource dataSource() {
-        return DataSourceBuilder.create()
-                .url("jdbc:mysql://localhost:3306/java")
-                .username("root")
-                .password("")
-                .driverClassName("com.mysql.cj.jdbc.Driver")
-                .build();
+        return DataSourceBuilder.create().build();
     }
 }
