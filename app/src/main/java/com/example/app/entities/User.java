f5bd285c2c03180fd2de63627dba7a4f890017ea
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-03-15 10:04:03 +0700

    :money_with_wings: add entities

---
+++ app/src/main/java/com/example/app/entities/User.java
@@ -0,0 +1,41 @@
+package com.example.app.entities;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import com.example.app.enumeration.Role;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+  
+    @Column(nullable = false, unique = true)
+    private String username;
+  
+    @Column(nullable = false)
+    private String password;
+  
+    @Column(unique = true)
+    private String email;
+  
+    @Column(name = "first_name")
+    private String firstName;
+  
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "tel")
+    private String tel;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+  
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+  
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
---
+++ app/src/main/java/com/example/app/enumeration/Role.java
@@ -0,0 +1,7 @@
+package com.example.app.enumeration;
+
+public enum Role {
+    ADMIN,
+    USER
+}
+
